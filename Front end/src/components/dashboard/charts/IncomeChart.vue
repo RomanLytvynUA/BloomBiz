62a873f68ce296dca3046be84616bde4e0de808b
RomanLytvyn <romanlytvynua@gmail.com>
2024-04-14 15:26:19 +0300

    BE > income statistic improvements

--- Front end/src/components/dashboard/charts/IncomeChart.vue
+++ Front end/src/components/dashboard/charts/IncomeChart.vue
@@ -1,142 +1,213 @@
 <template>
     <div>
-        <div class="row g-2 align-items-center justify-content-center">
-            <div class="col-auto">
-                <label for="incomeRangeSelect" class="col-form-label">Статистика прибутку за </label>
+        <div class="row g-2 justify-content-center">
+            <div class="col-auto d-flex align-items-center">
+                <p>Статистика прибутку за </p>
             </div>
             <div class="col-auto">
-                <select v-model="incomeRangeSelect" id="incomeRangeSelect" class="form-select form-select-sm">
+                <select v-model="incomeChartTypeSelect" class="form-select form-select-sm">
+                    <option value="day">День</option>
                     <option value="week">Тиждень</option>
+                    <option value="month">Місяць</option>
+                    <option value="quarter">Квартал</option>
                     <option value="year">Рік</option>
                 </select>
+            </div>
+            <div class="col-auto">
+                <DateFilter :type="incomeChartTypeSelect" ref="incomeChartDateSelect" @filterChanged="() => {
+                    incomeChartObj.data.labels = getIncomeChartLabels(); incomeChartObj.update(); setIncomeChartData();
+                }" />
             </div>
         </div>
         <canvas ref="incomeChart"></canvas>
     </div>
+    <!-- <button @click="console.log(getIncomeChartLabels())">---</button> -->
 </template>
 
 <script setup>
-import { ref, computed, onMounted, watch, watchEffect } from 'vue';
+import { ref, computed, onMounted, watch } from 'vue';
 import { useOrdersStore } from '../../../stores/orders';
 import { useExpensesStore } from '../../../stores/expenses';
+import { eachWeekOfInterval, format, eachDayOfInterval, getMonth, getYear, addMonths, eachYearOfInterval, isEqual, endOfYear, startOfDay, isSameWeek, eachMonthOfInterval, isSameMonth, eachQuarterOfInterval, isSameQuarter, isSameYear } from 'date-fns';
+import DateFilter from '../../table_elements/filters/DateFilter.vue';
 import Chart from 'chart.js/auto';
-
-const months = ['Січень',
-    'Лютий',
-    'Березень',
-    'Квітень',
-    'Травень',
-    'Червень',
-    'Липень',
-    'Серпень',
-    'Вересень',
-    'Жовтень',
-    'Листопад',
-    'Грудень'
-]
-function getCurrentWeekDates() {
-    let currentDate = new Date();
-    let currentDay = currentDate.getDay(); // 0 (Sunday) to 6 (Saturday)
-    let mondayOffset = currentDay === 0 ? -6 : 1 - currentDay; // Adjust offset for Sunday
-    let mondayDate = new Date(currentDate.setDate(currentDate.getDate() + mondayOffset));
-    let dates = [];
-
-    for (let i = 0; i < 7; i++) {
-        let date = new Date(mondayDate);
-        date.setDate(mondayDate.getDate() + i);
-        dates.push(date.getDate());
-    }
-
-    return dates;
-}
 
 let incomeChartObj = null
 const incomeChart = ref(null);
-const incomeRangeSelect = ref("week")
-const incomeLabels = computed(() => {
-    return incomeRangeSelect.value === 'week' ? getCurrentWeekDates() : months;
-});
-const incomeData = computed(() => {
+const ordersData = computed(() => useOrdersStore().ordersData)
+const expensesData = computed(() => useExpensesStore().expensesData)
+const incomeChartDateSelect = ref(null);
+const incomeChartTypeSelect = ref("day")
+const getIncomeChartLabels = () => {
     const data = []
-    if (incomeRangeSelect.value === 'year') {
-        const orders = useOrdersStore().ordersData.filter((order) => {
-            order.month = new Date(order.date).getMonth()
-            return new Date(order.date).getYear() === new Date().getYear()
-        })
-        const expenses = useExpensesStore().expensesData.filter((expense) => {
-            expense.month = new Date(expense.date).getMonth()
-            return new Date(expense.date).getYear() === new Date().getYear()
-        })
-        for (let month = 0; month <= 11; month++) {
-            let total = 0
-            orders.filter((order) => order.month === month).forEach(order => {
-                total += order.price
-            });
-            expenses.filter((expense) => expense.month === month).forEach(expense => {
-                total -= expense.total
-            });
-            data.push(total)
-        }
-    } else {
-        let currentDate = new Date();
-        currentDate.setHours(0, 0, 0, 0);
-        let currentDay = currentDate.getDay(); // 0 (Sunday) to 6 (Saturday)
-        let mondayOffset = currentDay === 0 ? -6 : 1 - currentDay; // Adjust offset for Sunday
-        const mondayDate = new Date(currentDate.setDate(currentDate.getDate() + mondayOffset));
-        const sundayDate = new Date(mondayDate);
-        sundayDate.setDate(mondayDate.getDate() + 6);
-        const orders = useOrdersStore().ordersData.filter((order) => {
-            const date = new Date(order.date)
-            order.day = new Date(order.date).getDay() === 0 ? 6 : new Date(order.date).getDay() - 1
-            return mondayDate <= new Date(date.getFullYear(), date.getMonth(), date.getDate()) && new Date(date.getFullYear(), date.getMonth(), date.getDate()) <= sundayDate;
-        })
-        const expenses = useExpensesStore().expensesData.filter((expense) => {
-            const date = new Date(expense.date)
-            expense.day = new Date(expense.date).getDay() === 0 ? 6 : new Date(expense.date).getDay() - 1
-            return mondayDate <= new Date(date.getFullYear(), date.getMonth(), date.getDate()) && new Date(date.getFullYear(), date.getMonth(), date.getDate()) <= sundayDate;
-        })
+    const chartType = incomeChartTypeSelect.value
+    const dateFilter = incomeChartDateSelect.value.getDate()
+    let date = dateFilter.fromDate
 
-        for (let day = 0; day <= 6; day++) {
-            let total = 0
-            orders.filter((order) => order.day === day).forEach(order => {
-                total += order.price
-            });
-            expenses.filter((expense) => expense.day === day).forEach(expense => {
-                total -= expense.total
-            });
-            data.push(total)
-        }
+    switch (chartType) {
+        case 'day':
+            eachDayOfInterval({ start: dateFilter.fromDate, end: dateFilter.toDate }).forEach(date => data.push(format(date, 'dd.MM.yyyy')))
+            return data
+        case 'week':
+            const [startYear, startWeek] = dateFilter.fromDate.split('-W');
+            const [endYear, endWeek] = dateFilter.toDate.split('-W');
+
+            const startDate = new Date(startYear, 0, (parseInt(startWeek) - 1) * 7 + 1);
+            const endDate = new Date(endYear, 0, (parseInt(endWeek) - 1) * 7 + 1);
+
+            const weeksInRange = eachWeekOfInterval({ start: startDate, end: endDate }, { weekStartsOn: 1 });
+            return weeksInRange.map(date => format(date, 'ww, yyyy'))
+        case 'month':
+            const months = ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень']
+            while (date <= dateFilter.toDate) {
+                data.push(`${months[getMonth(date)]} ${getYear(date)}`)
+                date = addMonths(date, 1)
+            }
+            return data
+        case 'quarter':
+            eachYearOfInterval({ start: dateFilter.fromDate, end: dateFilter.toDate }).forEach(year => {
+                data.push(`I, ${getYear(year)}`)
+                data.push(`II, ${getYear(year)}`)
+                data.push(`III, ${getYear(year)}`)
+                data.push(`IV, ${getYear(year)}`)
+            })
+            return data
+        case 'year':
+            eachYearOfInterval({ start: dateFilter.fromDate, end: dateFilter.toDate }).forEach(year => {
+                data.push(getYear(year))
+            })
+            return data
     }
-    return data
-});
+};
+const setIncomeChartData = () => {
+    const chartType = incomeChartTypeSelect.value
+    const dateFilter = incomeChartDateSelect.value.getDate()
+    const orders = ordersData.value.filter(order => incomeChartDateSelect.value.filterDate(order.date));
+    const expenses = expensesData.value.filter(expense => incomeChartDateSelect.value.filterDate(expense.date));
 
+    let expensesPrices = []
+    let ordersPrices = []
+    let incomePrices = []
+    switch (chartType) {
+        case 'day':
+            eachDayOfInterval({ start: dateFilter.fromDate, end: dateFilter.toDate }).forEach(date => {
+                const dayOrders = orders.filter(order => {
+                    return isEqual(startOfDay(new Date(order.date)), startOfDay(date))
+                })
+                const dayExpenses = expenses.filter(expense => {
+                    return isEqual(startOfDay(new Date(expense.date)), date)
+                })
+                const ordersPrice = dayOrders.reduce((total, order) => total + order.price, 0);
+                const expensesPrice = dayExpenses.reduce((total, expense) => total + expense.total, 0);
+                expensesPrices.push(expensesPrice)
+                ordersPrices.push(ordersPrice)
+                incomePrices.push(ordersPrice - expensesPrice)
+            })
+            break;
+        case 'week':
+            const [startYear, startWeek] = dateFilter.fromDate.split('-W');
+            const [endYear, endWeek] = dateFilter.toDate.split('-W');
+            const startDate = new Date(startYear, 0, (parseInt(startWeek) - 1) * 7 + 1);
+            const endDate = new Date(endYear, 0, (parseInt(endWeek) - 1) * 7 + 1);
+            eachWeekOfInterval({ start: startDate, end: endDate }, { weekStartsOn: 1 }).forEach(week => {
+                const weekOrders = orders.filter(order => {
+                    return isSameWeek(startOfDay(new Date(order.date)), startOfDay(week), { weekStartsOn: 1 })
+                })
+                const weekExpenses = expenses.filter(expense => {
+                    return isSameWeek(startOfDay(new Date(expense.date)), startOfDay(week), { weekStartsOn: 1 })
+                })
+                const ordersPrice = weekOrders.reduce((total, order) => total + order.price, 0);
+                const expensesPrice = weekExpenses.reduce((total, expense) => total + expense.total, 0);
+                expensesPrices.push(expensesPrice)
+                ordersPrices.push(ordersPrice)
+                incomePrices.push(ordersPrice - expensesPrice)
+            })
+            break;
+        case 'month':
+            eachMonthOfInterval({ start: dateFilter.fromDate, end: dateFilter.toDate }).forEach(month => {
+                const monthOrders = orders.filter(order => {
+                    return isSameMonth(startOfDay(new Date(order.date)), startOfDay(month))
+                })
+                const monthExpenses = expenses.filter(expense => {
+                    return isSameMonth(startOfDay(new Date(expense.date)), month)
+                })
+                const ordersPrice = monthOrders.reduce((total, order) => total + order.price, 0);
+                const expensesPrice = monthExpenses.reduce((total, expense) => total + expense.total, 0);
+                expensesPrices.push(expensesPrice)
+                ordersPrices.push(ordersPrice)
+                incomePrices.push(ordersPrice - expensesPrice)
+            })
+            break;
+        case "quarter":
+            eachQuarterOfInterval({ start: dateFilter.fromDate, end: endOfYear(dateFilter.toDate) }).forEach(quarter => {
+                const quarterOrders = orders.filter(order => {
+                    return isSameQuarter(startOfDay(new Date(order.date)), quarter)
+                })
+                const quarterExpenses = expenses.filter(expense => {
+                    return isSameQuarter(startOfDay(new Date(expense.date)), quarter)
+                })
+                const ordersPrice = quarterOrders.reduce((total, order) => total + order.price, 0);
+                const expensesPrice = quarterExpenses.reduce((total, expense) => total + expense.total, 0);
+                expensesPrices.push(expensesPrice)
+                ordersPrices.push(ordersPrice)
+                incomePrices.push(ordersPrice - expensesPrice)
+            })
+            break;
+        case "year":
+            eachYearOfInterval({ start: dateFilter.fromDate, end: endOfYear(dateFilter.toDate) }).forEach(year => {
+                const yearOrders = orders.filter(order => {
+                    return isSameYear(startOfDay(new Date(order.date)), year)
+                })
+                const yearExpenses = expenses.filter(expense => {
+                    return isSameYear(startOfDay(new Date(expense.date)), year)
+                })
+                const ordersPrice = yearOrders.reduce((total, order) => total + order.price, 0);
+                const expensesPrice = yearExpenses.reduce((total, expense) => total + expense.total, 0);
+                expensesPrices.push(expensesPrice)
+                ordersPrices.push(ordersPrice)
+                incomePrices.push(ordersPrice - expensesPrice)
+            })
+            break;
+    }
+    incomeChartObj.data.datasets[0].data = incomePrices
+    incomeChartObj.data.datasets[1].data = ordersPrices
+    incomeChartObj.data.datasets[2].data = expensesPrices
+    incomeChartObj.update()
+};
 
+watch([ordersData, expensesData], () => {
+    setIncomeChartData()
+}, { deep: true })
 onMounted(() => {
     const ctx = incomeChart.value.getContext('2d');
     incomeChartObj = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: incomeLabels.value,
+            labels: getIncomeChartLabels(),
             datasets: [{
-                data: incomeData.value,
+                label: 'Прибуток',
+                data: [],
                 fill: false,
-                borderColor: 'rgb(75, 192, 192)',
+                borderColor: '#36A2EB',
                 tension: 0.1,
-            }]
+            },
+            {
+                label: 'Дохід',
+                data: [],
+                fill: false,
+                borderColor: '#4BC0C0',
+                tension: 0.1,
+            },
+            {
+                label: 'Витрати',
+                data: [],
+                fill: false,
+                borderColor: '#FF6384',
+                tension: 0.1,
+            },
+            ]
         },
-        options: {
-            plugins: {
-                legend: {
-                    display: false
-                }
-            }
-        }
     })
+    setIncomeChartData();
 });
-
-watch(() => incomeRangeSelect.value, () => {
-    incomeChartObj.data.labels = incomeLabels.value
-    incomeChartObj.data.datasets[0].data = incomeData.value
-    incomeChartObj.update()
-}, { deep: true })
 </script>
--- Front end/src/components/table_elements/filters/DateFilter.vue
+++ Front end/src/components/table_elements/filters/DateFilter.vue
@@ -1,30 +1,160 @@
 <template>
-    <div class="input-group input-group-sm mb-3">
-        <span class="input-group-text">Від </span>
-        <input ref="fromDateFilter" type="date" class="form-control" :value="firstDayOfMonthDate" @input="$emit('filterChanged')">
-        <span class="input-group-text"> до </span>
-        <input ref="toDateFilter" type="date" class="form-control" :value="lastDayOfMonthDate" @input="$emit('filterChanged')">
+    <div>
+        <div v-if="type === 'year' || type === 'quarter'" class="input-group input-group-sm mb-3">
+            <span class="input-group-text">Від </span>
+            <select ref="fromYearFilter" class="form-select" @input="$emit('filterChanged')">
+                <option v-for="i in 10" :selected="type === 'quarter' ? i === 1 : i === 10"
+                    :value="new Date().getFullYear() - i + 1">{{ new
+            Date().getFullYear() - i + 1 }}
+                </option>
+            </select>
+            <span class="input-group-text"> до </span>
+            <select ref="toYearFilter" class="form-select" @input="$emit('filterChanged')">
+                <option v-for="i in 11" :selected="i === 0" :value="new Date().getFullYear() - i + 1">{{ new
+            Date().getFullYear() - i + 1 }}
+                </option>
+            </select>
+        </div>
+        <div v-else class="input-group input-group-sm mb-3">
+            <span class="input-group-text">Від </span>
+            <input ref="fromDateFilter" :type="type === 'day' ? 'date' : type" class="form-control"
+                :value="firstDefaultDate" @input="$emit('filterChanged')">
+            <span class="input-group-text"> до </span>
+            <input ref="toDateFilter" :type="type === 'day' ? 'date' : type" class="form-control"
+                :value="lastDefaultDate" @input="$emit('filterChanged')">
+        </div>
     </div>
 </template>
 
+
 <script>
+import { format, startOfMonth, endOfMonth, startOfWeek, endOfWeek, getWeek, getYear, isAfter, isBefore, isEqual, startOfDay, endOfYear, endOfDay } from 'date-fns';
+
 export default {
+    props: {
+        type: {
+            default: 'date'
+        }
+    },
     data() {
         return {
-            firstDayOfMonthDate: new Date().toISOString().slice(0, 7) + '-01',
-            lastDayOfMonthDate: new Date(new Date().getFullYear(), new Date().getMonth() + 1, 1).toISOString().split('T')[0],
+            firstDefaultDate: this.getDefaultFromDate(),
+            lastDefaultDate: this.getDefaultToDate(),
             fromDateFilter: null,
             toDateFilter: null,
+            fromYearFilter: null,
+            toYearFilter: null,
         };
+    },
+    watch: {
+        type() {
+            this.firstDefaultDate = this.getDefaultFromDate()
+            this.lastDefaultDate = this.getDefaultToDate()
+            this.$nextTick(() => {
+                this.$emit('filterChanged')
+            });
+        },
     },
     methods: {
         filterDate(rawDate) {
-            this.fromDateFilter = new Date(this.$refs.fromDateFilter.value);
-            this.toDateFilter = new Date(this.$refs.toDateFilter.value);
+            switch (this.type) {
+                case 'date':
+                    this.fromDateFilter = new Date(this.$refs.fromDateFilter.value);
+                    this.toDateFilter = new Date(this.$refs.toDateFilter.value);
+                    return (
+                        (isAfter(startOfDay(rawDate), startOfDay(this.fromDateFilter)) || isEqual(startOfDay(rawDate), startOfDay(this.fromDateFilter)) || this.fromDateFilter == 'Invalid Date') &&
+                        (isBefore(startOfDay(rawDate), startOfDay(this.toDateFilter)) || isEqual(startOfDay(rawDate), startOfDay(this.toDateFilter)) || this.toDateFilter == 'Invalid Date')
+                    );
+                case 'day':
+                    this.fromDateFilter = new Date(this.$refs.fromDateFilter.value);
+                    this.toDateFilter = new Date(this.$refs.toDateFilter.value);
+                    return (
+                        (isAfter(startOfDay(rawDate), startOfDay(this.fromDateFilter)) || isEqual(startOfDay(rawDate), startOfDay(this.fromDateFilter))) &&
+                        (isBefore(startOfDay(rawDate), startOfDay(this.toDateFilter)) || isEqual(startOfDay(rawDate), startOfDay(this.toDateFilter)))
+                    );
+                case 'week':
+                    this.fromDateFilter = this.$refs.fromDateFilter.value;
+                    this.toDateFilter = this.$refs.toDateFilter.value;
 
-            const date = new Date(rawDate);
+                    const [startYear, startWeek] = this.fromDateFilter.split('-W');
+                    const [endYear, endWeek] = this.toDateFilter.split('-W');
 
-            return (this.fromDateFilter <= date || this.fromDateFilter == 'Invalid Date') && (date <= this.toDateFilter || this.toDateFilter == 'Invalid Date')
+                    this.fromDateFilter = new Date(startYear, 0, (parseInt(startWeek) - 1) * 7 + 1);
+                    this.toDateFilter = new Date(endYear, 0, (parseInt(endWeek) - 1) * 7 + 7);
+                    return (
+                        (isAfter(startOfDay(rawDate), startOfDay(this.fromDateFilter)) || isEqual(startOfDay(rawDate), startOfDay(this.fromDateFilter))) &&
+                        (isBefore(startOfDay(rawDate), endOfDay(this.toDateFilter)) || isEqual(startOfDay(rawDate), startOfDay(this.toDateFilter)))
+                    );
+                case 'month':
+                    this.fromDateFilter = new Date(this.$refs.fromDateFilter.value);
+                    this.toDateFilter = new Date(this.$refs.toDateFilter.value);
+                    return (
+                        (isAfter(startOfDay(rawDate), startOfDay(this.fromDateFilter)) || isEqual(startOfDay(rawDate), startOfDay(this.fromDateFilter))) &&
+                        (isBefore(startOfDay(rawDate), endOfDay(this.toDateFilter)) || isEqual(startOfDay(rawDate), startOfDay(this.toDateFilter)))
+                    );
+                case 'quarter':
+                    this.fromDateFilter = new Date(this.$refs.fromYearFilter.value);
+                    this.toDateFilter = endOfYear(new Date(this.$refs.toYearFilter.value));
+                    return (
+                        (isAfter(startOfDay(rawDate), startOfDay(this.fromDateFilter)) || isEqual(startOfDay(rawDate), startOfDay(this.fromDateFilter))) &&
+                        (isBefore(startOfDay(rawDate), endOfDay(this.toDateFilter)) || isEqual(startOfDay(rawDate), startOfDay(this.toDateFilter)))
+                    );
+                case 'year':
+                    this.fromDateFilter = new Date(this.$refs.fromYearFilter.value);
+                    this.toDateFilter = endOfYear(new Date(this.$refs.toYearFilter.value));
+                    return (
+                        (isAfter(startOfDay(rawDate), startOfDay(this.fromDateFilter)) || isEqual(startOfDay(rawDate), startOfDay(this.fromDateFilter))) &&
+                        (isBefore(startOfDay(rawDate), endOfDay(this.toDateFilter)) || isEqual(startOfDay(rawDate), startOfDay(this.toDateFilter)))
+                    );
+            }
+        },
+        getDate() {
+            switch (this.type) {
+                case 'date':
+                    return { fromDate: new Date(this.$refs.fromDateFilter.value), toDate: new Date(this.$refs.toDateFilter.value) };
+                case 'day':
+                    return { fromDate: new Date(this.$refs.fromDateFilter.value), toDate: new Date(this.$refs.toDateFilter.value) };
+                case 'week':
+                    return { fromDate: this.$refs.fromDateFilter.value, toDate: this.$refs.toDateFilter.value };
+                case 'month':
+                    return { fromDate: new Date(this.$refs.fromDateFilter.value), toDate: new Date(this.$refs.toDateFilter.value) };
+                case 'quarter':
+                    return { fromDate: new Date(this.$refs.fromYearFilter.value), toDate: new Date(this.$refs.toYearFilter.value) };
+                case 'year':
+                    return { fromDate: new Date(this.$refs.fromYearFilter.value), toDate: new Date(this.$refs.toYearFilter.value) };
+                default:
+                    return {};
+            }
+        },
+        getDefaultFromDate() {
+            const currentDate = new Date();
+            switch (this.type) {
+                case 'date':
+                    return format(startOfMonth(currentDate), 'yyyy-MM-dd');
+                case 'day':
+                    return format(startOfWeek(currentDate, { weekStartsOn: 1 }), 'yyyy-MM-dd');
+                case 'week':
+                    return `${getYear(currentDate)}-W${getWeek(startOfMonth(currentDate), { weekStartsOn: 1 }).toString().padStart(2, '0')}`
+                case 'month':
+                    return getYear(new Date()) + '-01';
+                default:
+                    return '';
+            }
+        },
+        getDefaultToDate() {
+            const currentDate = new Date();
+            switch (this.type) {
+                case 'date':
+                    return format(endOfMonth(currentDate), 'yyyy-MM-dd');
+                case 'day':
+                    return format(endOfWeek(currentDate, { weekStartsOn: 1 }), 'yyyy-MM-dd');
+                case 'week':
+                    return `${getYear(currentDate)}-W${getWeek(endOfMonth(currentDate), { weekStartsOn: 1 }).toString().padStart(2, '0')}`
+                case 'month':
+                    return getYear(new Date()) + '-12';
+                default:
+                    return '';
+            }
         },
     },
 };

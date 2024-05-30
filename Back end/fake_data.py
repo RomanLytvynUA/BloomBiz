d16df4e98dacf93dd6d22fa4a69e53cce18806ec
RomanLytvyn <romanlytvynua@gmail.com>
2024-05-30 10:54:45 +0300

    BE > fake_data fix

--- Back end/fake_data.py
+++ Back end/fake_data.py
@@ -6,6 +6,7 @@ from src.models.goods import *
 from src.models.suppliers import *
 from src.models.expenses import *
 from src.models.orders import *
+from src.utils.settings import util_reset_settings
 
 
 def add_categories():
@@ -264,6 +265,8 @@ def add_orders_elements():
 with app.app_context():
     db.drop_all()
     db.create_all()
+
+    util_reset_settings()
     add_categories()
     add_goods()
     add_suppliers()

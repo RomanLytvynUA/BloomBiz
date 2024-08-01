1215ecdbdd7370c9bd684f717503978744c16427
RomanLytvyn <romanlytvynua@gmail.com>
2024-08-01 16:17:31 +0300

    BE > automatic soft db reset only works when debuger is on

--- Back end/run.py
+++ Back end/run.py
@@ -2,6 +2,7 @@ from src import app
 from db_setup import soft_db_setup
 
 if __name__ == "__main__":
-    # set up missing tables if there are any
-    soft_db_setup()
-    app.run(host="0.0.0.0", debug=True)
+    # set up missing tables if there are any and debuger in on
+    if app.debug:
+        soft_db_setup()
+    app.run(host="0.0.0.0")
